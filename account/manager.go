d61caa1d87293d1ab5e98e2b77d63eb2e41731fe
Viktoras Kuznecovas <viktoras@kuznecovas.net>
2021-09-09 14:12:05 +0300

    Add ownership verification to account creation

--- account/manager.go
+++ account/manager.go
@@ -11,23 +11,39 @@ type accountStorage interface {
 }
 
 type Manager struct {
-	storage accountStorage
+	storage           accountStorage
+	ownershipVerifier OwnershipVerifier
 }
 
-func NewManager(storage accountStorage) *Manager {
+func NewManager(storage accountStorage, ownershipVerifier OwnershipVerifier) *Manager {
 	return &Manager{
-		storage: storage,
+		storage:           storage,
+		ownershipVerifier: ownershipVerifier,
 	}
 }
 
+type OwnershipVerifier interface {
+	GenerateAndVerify(acc AccountID, signature []byte, version string) error
+}
+
+func (m *Manager) RegisterAccount(accountID AccountID, ownershipClaim OwnershipClaim) (Account, error) {
+	err := m.ownershipVerifier.GenerateAndVerify(accountID, ownershipClaim.Signature, ownershipClaim.Claim.Domain.Version)
+	if err != nil {
+		return Account{}, err
+	}
+
+	return m.CreateAccount(accountID, ownershipClaim)
+}
+
 // Validation of ownership is up to the caller
-func (m *Manager) CreateAccount(accountID AccountID) (Account, error) {
+func (m *Manager) CreateAccount(accountID AccountID, ownershipClaim OwnershipClaim) (Account, error) {
 	acc, err := m.GetAccount(accountID)
 	if err != nil && !errors.Is(err, ErrNotFound) {
 		return acc, err
 	}
 	return m.storage.SaveAccount(Account{
-		ID: accountID.String(),
+		ID:             accountID.String(),
+		OwnershipClaim: ownershipClaim,
 	})
 }
 
